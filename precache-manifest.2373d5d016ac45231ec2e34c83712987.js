e4c62c74ed2726781a3ebdc4847fe5d00644ff48
spacecowboyian <ian@futurehat.com>
2020-08-19 13:44:39 -0500

    Updates

---
+++ precache-manifest.2373d5d016ac45231ec2e34c83712987.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019ca044850687ef61952b960f589b7c",
+    "url": "/edstest/index.html"
+  },
+  {
+    "revision": "f9f46f10b28deea85d58",
+    "url": "/edstest/static/css/2.db164f65.chunk.css"
+  },
+  {
+    "revision": "0a1c9f9c60d539e3b11f",
+    "url": "/edstest/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f9f46f10b28deea85d58",
+    "url": "/edstest/static/js/2.1f9fceb2.chunk.js"
+  },
+  {
+    "revision": "826c1dfb662e1c2b5d4d377600bdc9a9",
+    "url": "/edstest/static/js/2.1f9fceb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a1c9f9c60d539e3b11f",
+    "url": "/edstest/static/js/main.6ee2c95c.chunk.js"
+  },
+  {
+    "revision": "7111e8e55570370c7f95",
+    "url": "/edstest/static/js/runtime-main.9272f700.js"
+  }
+]);
